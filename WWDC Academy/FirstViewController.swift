81274287de59d27f3fe2c8118e0ad517c6072239
slade9220 <slade9220@gmail.com>
2018-06-01 11:55:55 +0200

    Initial Commit

---
+++ WWDC Academy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WWDC Academy
+//
+//  Created by Gennaro Amura on 01/06/18.
+//  Copyright © 2018 Gennaro Amura. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
